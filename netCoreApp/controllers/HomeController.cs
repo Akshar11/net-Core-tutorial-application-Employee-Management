f14e588042144a2a76cff5c18ba59411640c675c
Akshar <nimesh1819@gmail.com>
2020-07-25 16:13:15 +0530

    Add project files.

---
+++ netCoreApp/controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using netCoreApp.models;
+
+namespace netCoreApp.controllers
+{
+    public class HomeController : Controller
+    {
+        private IEmployeeRepository _employeeRepository;
+
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+        public string Index()
+        {
+            return _employeeRepository.GetEmployeeById(1).Name;
+        }
+        public JsonResult details() {
+            Employee model = _employeeRepository.GetEmployeeById(1);
+            return Json(model);
+        }
+    }
+}
---
+++ netCoreApp/models/MockEmployeeRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace netCoreApp.models
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> _employeeList;
+
+        public MockEmployeeRepository() {
+            _employeeList =new List<Employee>(){
+                new Employee() { Id = 1,Department="bhajan",Email="a@a.com",Name="n1" },
+                    new Employee() { Id = 2, Department = "bhajan", Email = "a@b.com", Name = "n2" },
+                    new Employee() { Id = 3, Department = "bhajan", Email = "a@c.com", Name = "n3" },
+            };
+        }
+
+        public Employee GetEmployeeById(int id)
+        {
+          return   _employeeList.FirstOrDefault(e=>e.Id==id);
+        }
+    }
+}
