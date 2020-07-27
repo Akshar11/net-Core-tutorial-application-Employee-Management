c074e4eb9b71c4079b86798d2f5739c50d239932
Akshar <nimesh1819@gmail.com>
2020-07-27 15:24:30 +0530

    EP:31  _view start

---
+++ netCoreApp/viewModels/HomeDetailViewModel.cs
@@ -0,0 +1,15 @@
+﻿using netCoreApp.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace netCoreApp.viewModels
+{
+    public class HomeDetailViewModel
+    {
+        public Employee Employee { get; set; }
+        public string title { get; set; }
+
+    }
+}
